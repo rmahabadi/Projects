fa51a17c0998bb8ce54163d467caf5848082d56d
Rostam Mahabadi <rostammahabadi@Rostams-MacBook-Pro.local>
2020-01-26 18:30:20 -0700

    Pull from finviz, second page due to format differentiation

--- Algo.py
+++ Algo.py
@@ -13,3 +13,17 @@ from statistics import mean,median,mode
 Ticker=[]
 Symbol=[]
 Alphavantage_API=[]
+
+for i in range(11,21,10):
+    #Skipped first page due to format difference in URL after first page
+    Top_Gainers_URL= 'https://finviz.com/screener.ashx?v=340&s=ta_topgainers&r='+str(i)
+    r= requests.get(Top_Gainers_URL)
+    data=r.text
+    soup=BeautifulSoup(data,features="lxml")
+    for tickers in soup.find_all('td',attrs={'class':'snapshot-td'}):
+        for name in tickers.find_all(attrs={'class':'tab-link'}):
+            tick = name.text
+            s = re.findall("([A-Z]{2,4})", tick)
+            for word in s:
+                if word != "USA":
+                    Ticker.append(word)
