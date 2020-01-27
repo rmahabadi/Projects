117defc33e30275ba26985911389ce65beb7cab5
Rostam Mahabadi <rostammahabadi@Rostams-MacBook-Pro.local>
2020-01-26 18:16:42 -0700

    created Algo.py

---
+++ Algo.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pandas_datareader as dr
+from matplotlib import pyplot
+#%matplotlib inline
+from datetime import datetime   
+import urllib.request
+from bs4 import BeautifulSoup
+import requests
+import re
+import datetime
+from statistics import mean,median,mode
